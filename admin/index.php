70d15c92e30c91a8b112e870def4d7240f9c517d
dillafrose <144106074+dillafrose@users.noreply.github.com>
2025-12-28 21:28:16 +0600

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -3,25 +3,28 @@ session_start();
 include('includes/config.php');
 if(isset($_POST['login']))
 {
-$uname=$_POST['username'];
-$password=md5($_POST['password']);
-$sql ="SELECT UserName,Password FROM admin WHERE UserName=:uname and Password=:password";
-$query= $dbh -> prepare($sql);
-$query-> bindParam(':uname', $uname, PDO::PARAM_STR);
-$query-> bindParam(':password', $password, PDO::PARAM_STR);
-$query-> execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-if($query->rowCount() > 0)
-{
-$_SESSION['alogin']=$_POST['username'];
-echo "<script type='text/javascript'> document.location = 'dashboard.php'; </script>";
-} else{
-	
-	echo "<script>alert('Invalid Details');</script>";
+    $uname = trim($_POST['username']);
+    $password = trim($_POST['password']);
 
+    $sql ="SELECT UserName, Password FROM admin 
+           WHERE UserName=:uname AND Password=:password";
+
+    $query = $dbh->prepare($sql);
+    $query->bindParam(':uname', $uname, PDO::PARAM_STR);
+    $query->bindParam(':password', $password, PDO::PARAM_STR);
+    $query->execute();
+
+    if($query->rowCount() > 0)
+    {
+        $_SESSION['alogin'] = $uname;
+        echo "<script>document.location='dashboard.php';</script>";
+    }
+    else
+    {
+        echo "<script>alert('Invalid Details');</script>";
+    }
 }
 
-}
 
 ?>
 
