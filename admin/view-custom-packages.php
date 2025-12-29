55170b4caabc12e4e23987b3e04d8c29c66ee23d
dillafrose <144106074+dillafrose@users.noreply.github.com>
2025-12-29 11:51:53 +0600

    Change page title from 'GoPlan Tours' to 'TripIN'
    Updated the title in the HTML head section from 'GoPlan Tours - Custom Packages' to 'TripIN - Custom Packages'.

--- admin/view-custom-packages.php
+++ admin/view-custom-packages.php
@@ -42,7 +42,7 @@ if(strlen($_SESSION['alogin']) == 0) {
 <!DOCTYPE HTML>
 <html>
 <head>
-<title>GoPlan Tours - Custom Packages</title>
+<title>TripIN - Custom Packages</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
